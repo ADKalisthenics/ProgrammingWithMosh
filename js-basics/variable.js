8463cb0f30dd4b39624a3a473af139d338de10ca
Dokyung An <dokyungdavidan@gmail.com>
2022-12-21 16:21:55 +0900

    Added variable js file

---
+++ js-basics/variable.js
@@ -0,0 +1,11 @@
+let name = 'Dokyung';
+console.log(name);
+
+//Cannot be a reserved keyword
+//Should be meaningful
+//Cannot start with a number (1name) x
+//Cannot contain a space or hyphen(-)
+//Are case-sensitive
+
+let firstName = 'Someone', lastName = 'Else';
+let FirstName;
